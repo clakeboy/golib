c0da01601f38101e908d5d64fcec872b655a8840
clakeboy <clakeboy@gmail.com>
2017-11-23 16:52:51 +0800

    http 添加COOKIE方法

--- utils/newhttp.go
+++ utils/newhttp.go
@@ -8,8 +8,9 @@ import (
 	"time"
 	"io"
 	"fmt"
+	"net/http/cookiejar"
 )
-
+//http 请求返回数据
 type HttpRequestData struct {
 	Status  string
 	StatusCode int
@@ -17,14 +18,14 @@ type HttpRequestData struct {
 	Content []byte
 	Cookie *HttpCookies
 }
-
+//http 请求客户端
 type HttpClient struct {
 	client  *http.Client
 	headers M
 	cookies []*http.Cookie
 	lastRequest *HttpRequestData
 }
-
+//cookie集合处理
 type HttpCookies struct {
 	Cookies []*http.Cookie
 }
@@ -43,13 +44,15 @@ func (hc *HttpCookies) GetCookieString() string {
 
 	return strings.Join(arr,"; ")
 }
-
+//创建一个新的HTTP连接客户端
 func NewHttpClient() *HttpClient {
+	jar, _ := cookiejar.New(nil)
 	client := &http.Client{}
 	client.Timeout = time.Second * 30
+	client.Jar = jar
 	return &HttpClient{client: client, headers: M{},cookies:[]*http.Cookie{}}
 }
-
+//发起一个POST请求
 func (h *HttpClient) Post(url_str string, data M) ([]byte, error) {
 	h.SetHeader("Content-Type","application/x-www-form-urlencoded")
 	post_data := &url.Values{}
@@ -66,7 +69,7 @@ func (h *HttpClient) Post(url_str string, data M) ([]byte, error) {
 
 	return req.Content, nil
 }
-
+//发起一个POST JSON请求
 func (h *HttpClient) PostJson(url_str string,data M) ([]byte,error) {
 	h.SetHeader("Content-Type","application/json")
 	req, err := h.Request("POST",url_str,strings.NewReader(data.ToJsonString()))
@@ -76,7 +79,7 @@ func (h *HttpClient) PostJson(url_str string,data M) ([]byte,error) {
 
 	return req.Content, nil
 }
-
+//发起一个POST XML请求
 func (h *HttpClient) PostXml(url_str string,data string) ([]byte,error) {
 	h.SetHeader("Content-Type","text/xml;charset=utf-8")
 	req, err := h.Request("POST",url_str,strings.NewReader(data))
@@ -86,7 +89,7 @@ func (h *HttpClient) PostXml(url_str string,data string) ([]byte,error) {
 
 	return req.Content, nil
 }
-
+//发起一个GET请求
 func (h *HttpClient) Get(url_str string) ([]byte, error) {
 	resp,err := h.Request("GET",url_str,nil)
 	if err != nil {
@@ -94,7 +97,7 @@ func (h *HttpClient) Get(url_str string) ([]byte, error) {
 	}
 	return resp.Content,nil
 }
-
+//发起一个http request 请求
 func (h *HttpClient) Request(method string,url_str string,content io.Reader) (*HttpRequestData,error) {
 	req,err := http.NewRequest(method,url_str,content)
 	if err != nil {
@@ -133,27 +136,27 @@ func (h *HttpClient) Request(method string,url_str string,content io.Reader) (*H
 	h.lastRequest = res
 	return res,nil
 }
-
+//设置请求头信息
 func (h *HttpClient) SetHeader(key, val string) {
 	h.headers[key] = val
 }
-
+//设置请求超时时间
 func (h *HttpClient) SetTimeout(sc time.Duration) {
 	h.client.Timeout = sc
 }
-
+//设置请求带的COOKIE信息
 func (h *HttpClient) SetCookie(cookie *http.Cookie) {
 	if cookie == nil {
 		return
 	}
 	h.cookies = append(h.cookies,cookie)
 }
-
+//清除HTTP当前请求的数据
 func (h *HttpClient) Clear() {
 	h.cookies = []*http.Cookie{}
 	h.headers = M{}
 }
-
+//得到最后一次请求的返回数据
 func (h *HttpClient) GetLastResponse() *HttpRequestData {
 	return h.lastRequest
 }
