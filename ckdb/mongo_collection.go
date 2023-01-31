ac17375b7949b423a5ad21e2ab6e8785d1f7825a
clakeboy <clakeboy@gmail.com>
2023-01-31 17:28:22 +0800

    moogo driver add allowdiskuse option

--- ckdb/mongo_collection.go
+++ ckdb/mongo_collection.go
@@ -7,7 +7,7 @@ import (
 	"reflect"
 )
 
-//查询结果
+// 查询结果
 type QueryResult struct {
 	List  interface{} `json:"list"`
 	Count int         `json:"count"`
@@ -22,7 +22,7 @@ func NewCollection(mdb *DBMongo, tab_name string) *CKCollection {
 	return &CKCollection{db: mdb, tab: tab_name}
 }
 
-//插入数据
+// Insert 插入数据
 func (ck *CKCollection) Insert(rows ...interface{}) error {
 	c := ck.db.Table(ck.tab)
 	err := c.Insert(rows...)
@@ -32,7 +32,7 @@ func (ck *CKCollection) Insert(rows ...interface{}) error {
 	return nil
 }
 
-//更新数据
+// Update 更新数据
 func (ck *CKCollection) Update(where bson.M, update interface{}) error {
 	c := ck.db.Table(ck.tab)
 	err := c.Update(where, update)
@@ -42,7 +42,7 @@ func (ck *CKCollection) Update(where bson.M, update interface{}) error {
 	return nil
 }
 
-//更新所有条件数据
+// UpdateAll 更新所有条件数据
 func (ck *CKCollection) UpdateAll(where bson.M, update interface{}) (*mgo.ChangeInfo, error) {
 	c := ck.db.Table(ck.tab)
 	return c.UpdateAll(where, update)
@@ -58,7 +58,7 @@ func (ck *CKCollection) Upset(where bson.M, update interface{}) error {
 	return nil
 }
 
-//删除数据
+// Delete 删除数据
 func (ck *CKCollection) Delete(where bson.M) error {
 	c := ck.db.Table(ck.tab)
 	_, err := c.RemoveAll(where)
@@ -68,7 +68,7 @@ func (ck *CKCollection) Delete(where bson.M) error {
 	return nil
 }
 
-//查找数据
+// 查找数据
 func (ck *CKCollection) Find(where bson.M, row interface{}) error {
 	c := ck.db.Table(ck.tab)
 	err := c.Find(where).One(row)
@@ -78,7 +78,7 @@ func (ck *CKCollection) Find(where bson.M, row interface{}) error {
 	return nil
 }
 
-//得到所给条件的数据量
+// Count 得到所给条件的数据量
 func (ck *CKCollection) Count(where bson.M) int {
 	c := ck.db.Table(ck.tab)
 	count, err := c.Find(where).Count()
@@ -88,7 +88,7 @@ func (ck *CKCollection) Count(where bson.M) int {
 	return count
 }
 
-//查询数据库
+// Query 查询数据库
 func (ck *CKCollection) Query(where bson.M, page int, number int, sort_list []string, struct_type interface{}, format func(interface{})) (*QueryResult, error) {
 	c := ck.db.Table(ck.tab)
 	var list []interface{}
@@ -124,7 +124,7 @@ func (ck *CKCollection) Query(where bson.M, page int, number int, sort_list []st
 	return res, err
 }
 
-//查询数据库返回列表
+// List 查询数据库返回列表
 func (ck *CKCollection) List(where bson.M, page int, number int, sort_list []string, struct_type interface{}, format func(interface{})) ([]interface{}, error) {
 	c := ck.db.Table(ck.tab)
 	var list []interface{}
@@ -152,12 +152,25 @@ func (ck *CKCollection) List(where bson.M, page int, number int, sort_list []str
 	return list, err
 }
 
-//执行聚合操作
+// Aggregate 执行聚合操作
 func (ck *CKCollection) Aggregate(pipe ...bson.M) (bson.M, error) {
 	c := ck.db.Table(ck.tab)
 	p := c.Pipe(pipe)
 	resp := bson.M{}
 	err := p.One(&resp)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+// AggregateAllowDiskUse 执行聚合操作 allowDiskUse
+func (ck *CKCollection) AggregateAllowDiskUse(pipe ...bson.M) (bson.M, error) {
+	c := ck.db.Table(ck.tab)
+	p := c.Pipe(pipe)
+	resp := bson.M{}
+	err := p.AllowDiskUse().One(&resp)
 	if err != nil {
 		return nil, err
 	}
@@ -177,12 +190,12 @@ func (ck *CKCollection) getQueryType(i interface{}) interface{} {
 	return reflect.New(t).Interface()
 }
 
-//删除 Collection
+// Drop 删除 Collection
 func (ck *CKCollection) Drop() error {
 	return ck.db.Table(ck.tab).DropCollection()
 }
 
-//关闭数据库连接
+// Close 关闭数据库连接
 func (ck *CKCollection) Close() {
 	ck.db.Close()
 }
