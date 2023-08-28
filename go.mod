fc9dfea01269cfe59ce3be8b3eb579fdcf91ea74
Clake <clakeboy@gmail.com>
2023-08-28 13:49:56 +0800

    修改mongo 官方驱动到 1.21.1

--- go.mod
+++ go.mod
@@ -7,7 +7,7 @@ require (
 	github.com/gin-gonic/gin v1.4.0
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/gomodule/redigo v1.8.9
-	go.mongodb.org/mongo-driver v1.0.1
+	go.mongodb.org/mongo-driver v1.12.1
 	golang.org/x/crypto v0.11.0
 	golang.org/x/text v0.11.0
 	gopkg.in/mgo.v2 v2.0.0-20180705113604-9856a29383ce
@@ -15,23 +15,23 @@ require (
 
 require (
 	github.com/baiyubin/aliyun-sts-go-sdk v0.0.0-20180326062324-cfa1a18b161f // indirect
-	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gin-contrib/sse v0.0.0-20190301062529-5545eab6dad3 // indirect
-	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/golang/protobuf v1.3.1 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
-	github.com/google/go-cmp v0.2.0 // indirect
 	github.com/json-iterator/go v1.1.6 // indirect
+	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/mattn/go-isatty v0.0.7 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
 	github.com/satori/go.uuid v1.2.0 // indirect
-	github.com/tidwall/pretty v0.0.0-20180105212114-65a9db5fad51 // indirect
 	github.com/ugorji/go v1.1.4 // indirect
-	github.com/xdg/scram v0.0.0-20180814205039-7eeb5667e42c // indirect
-	github.com/xdg/stringprep v1.0.0 // indirect
-	golang.org/x/sync v0.0.0-20181221193216-37e7f081c4d4 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.1.2 // indirect
+	github.com/xdg-go/stringprep v1.0.4 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	golang.org/x/sync v0.0.0-20220722155255-886fb9371eb4 // indirect
 	golang.org/x/sys v0.10.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 	gopkg.in/go-playground/validator.v8 v8.18.2 // indirect
--- mongo/database.go
+++ mongo/database.go
@@ -3,14 +3,15 @@ package mongo
 import (
 	"context"
 	"fmt"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
-	"gopkg.in/mgo.v2/bson"
-	"time"
 )
 
-//dsn mongodb://root:WiaQ82n7B3L5Cz*2#10m@172.18.76.150:27017?authSource=admin
+// dsn mongodb://root:WiaQ82n7B3L5Cz*2#10m@172.18.76.150:27017?authSource=admin
 type Config struct {
 	Host     string `json:"host" yaml:"host"`
 	Port     string `json:"port" yaml:"port"`
@@ -21,7 +22,7 @@ type Config struct {
 	PoolSize int    `json:"pool_size" yaml:"pool_size"`
 }
 
-//mongodb orm use official driver
+// mongodb orm use official driver
 type Database struct {
 	client        *mongo.Client
 	currentDBName string
@@ -30,8 +31,9 @@ type Database struct {
 func NewDatabase(conf *Config) (*Database, error) {
 	opts := options.Client()
 	opts.SetHosts([]string{fmt.Sprintf("%s:%s", conf.Host, conf.Port)})
-	opts.SetMaxPoolSize(uint16(conf.PoolSize))
-	opts.SetConnectTimeout(20 * time.Second)
+	opts.SetMaxPoolSize(uint64(conf.PoolSize))
+	opts.SetConnectTimeout(5 * time.Second)
+	opts.SetTimeout(30 * time.Second)
 	if conf.Auth != "" {
 		opts.SetAuth(options.Credential{
 			AuthMechanism: "SCRAM-SHA-1",
@@ -41,8 +43,7 @@ func NewDatabase(conf *Config) (*Database, error) {
 			PasswordSet:   true,
 		})
 	}
-
-	client, err := mongo.NewClient(opts)
+	client, err := mongo.Connect(context.TODO(), opts)
 	if err != nil {
 		return nil, err
 	}
@@ -53,36 +54,30 @@ func NewDatabase(conf *Config) (*Database, error) {
 	}, nil
 }
 
-//connect to mongodb
+// connect to mongodb
 func (d *Database) Open() error {
-	ctx, _ := context.WithTimeout(context.Background(), time.Second*5)
-	err := d.client.Connect(ctx)
-	if err != nil {
-		return err
-	}
-
 	return d.Ping()
 }
 
-//disconnect mongodb
+// disconnect mongodb
 func (d *Database) Close() error {
 	return d.client.Disconnect(context.Background())
 }
 
-//select database
+// select database
 func (d *Database) SelectDatabase(dbName string) {
 	d.currentDBName = dbName
 }
 
 func (d *Database) ListDatabase() (mongo.ListDatabasesResult, error) {
-	return d.client.ListDatabases(nil, bson.M{})
+	return d.client.ListDatabases(context.Background())
 }
 
 func (d *Database) ListDatabaseNames() ([]string, error) {
-	return d.client.ListDatabaseNames(nil, bson.M{})
+	return d.client.ListDatabaseNames(context.Background(), bson.D{})
 }
 
-//get default database
+// get default database
 func (d *Database) Database(dbName ...string) *mongo.Database {
 	if len(dbName) > 0 {
 		return d.client.Database(dbName[0])
@@ -90,7 +85,7 @@ func (d *Database) Database(dbName ...string) *mongo.Database {
 	return d.client.Database(d.currentDBName)
 }
 
-//ping mongodb server
+// ping mongodb server
 func (d *Database) Ping() error {
 	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
 	return d.client.Ping(ctx, readpref.Primary())
